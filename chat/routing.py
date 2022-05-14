03a5a49030a86fb45f5396262c0cd6a62ae1b4bd
Daniel Ares Moraes <daniel.ares@hotmail.com>
2022-05-14 09:07:50 -0300

    deploy heroku 2

--- chat/routing.py
+++ chat/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from .consumers import ChatConsumer
 
 websocket_urlpatterns = [
-    re_path(r'wss/chat/(?P<nome_sala>\w+)/$', ChatConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<nome_sala>\w+)/$', ChatConsumer.as_asgi()),
 ]
