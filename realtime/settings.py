ca982debc57f541703bb96c62adcace36e501048
Daniel Ares Moraes <daniel.ares@hotmail.com>
2022-05-14 10:07:21 -0300

    fixing https error

--- realtime/settings.py
+++ realtime/settings.py
@@ -131,6 +131,7 @@ CHANNEL_LAYERS = {
         "CONFIG": {
             "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
         },
+        'ROUTING': 'realtime.routing.channel_routing',
     },
 }
 
